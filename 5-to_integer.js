2797b12661ea4beebcdfd086ac757b73412ceb14
Nnamdi Charles Okonkwo <nnamdi212@gmail.com>
2025-05-28 01:14:45 +0100

    Add a script that prints my number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,8 @@
-const args = process.argv.slice(2); // Get the command line arguments, excluding the first two
+const arg = process.argv[2]; // Get the first argument from the command line
+const number = Number(arg); // Try to convert it to a number
 
-const firstArg = args[0];
-const num = Number(firstArg); // Convert the argument to a number
-
-console.log((num == firstArg) ? `My number: ${num}
+if (!isNaN(number)) {
+  console.log("My number:", number);
+} else {
+  console.log("Not a number");
+}
