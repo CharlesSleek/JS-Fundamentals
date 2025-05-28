59b63d0a882cbb6d6947306201348c09467fb5f0
Nnamdi Charles Okonkwo <nnamdi212@gmail.com>
2025-05-28 01:32:48 +0100

    Added a script that prints a factorial

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+const n = Number(process.argv[2]);
+
+function factorial(num) {
+  if (isNaN(num)) return 1;
+  if (num <= 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(n));
